857fa762cf722b15161d7597b8e76656d9616599
charlie <charliechengkn@hotmail.com>
2024-08-03 22:37:18 +0800

    regular updates

--- tools/plotting.py
+++ tools/plotting.py
@@ -1,11 +1,19 @@
 import matplotlib.pyplot as plt
 import math
 
-def get_size(size: int, row_number: int):
-    # given an integer n, returns the size of the subplots needed
-    return tuple(math.ceil(size/ row_number), row_number)
 
-def get_location(i: int, shape: tuple):
-    # shape is a tuple in (m, n)
-    assert i > shape[0] * shape[1], "i must be smaller than size m x n"
-    return math.ceil(i/shape[1]), i % shape[1]
+class Plot:
+    def scatter(*arg):
+        plt.scatter(arg)
+    pass
+
+class Multiplots:
+
+    def __init__(self, shape):
+        self.size = shape[0] * shape[1]
+    
+    def get_location(i: int, shape: tuple):
+        # shape is a tuple in (m, n)
+        rows, cols = shape
+        assert i > rows * cols, "i must be smaller than size m x n"
+        return math.ceil(i/shape[1]), i % shape[1]
