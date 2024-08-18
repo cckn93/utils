4a145ceca3402f447cbea455d3a39e5d3e280c56
charlie <charliechengkn@hotmail.com>
2024-08-18 15:54:43 +0800

    dev

--- tools/data_dump.py
+++ tools/data_dump.py
@@ -1,6 +1,11 @@
 # module to download data
 import pandas as pd
 import yfinance as yf
+from datetime import date
 
-def get_closing(ticker: str, start: str=None, end: str=None) -> pd.DataFrame:
-    return yf.Ticker(ticker).history(start=start, end=end)
+START_DATE = date(2016, 12, 31)
+
+def get_closing(ticker: str, start: str=START_DATE, end: str=None) -> pd.DataFrame:
+    end = end or date.today()
+    prices =  yf.Ticker(ticker).history(start=start, end=end)
+    prices.to_csv('sample_date/{}.csv'.format(ticker))
